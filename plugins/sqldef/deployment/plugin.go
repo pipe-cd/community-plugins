11cb13911b94fa1e463399cc5d3abd17db9442e4
kadai0308 <kadai0308@gmail.com>
2025-07-19 16:37:54 +1000

    feat(sqldef) implement build stages func for sqldef plugin
    Signed-off-by: kadai0308 <kadai0308@gmail.com>

--- plugins/sqldef/deployment/plugin.go
+++ plugins/sqldef/deployment/plugin.go
@@ -2,10 +2,19 @@ package deployment
 
 import (
 	"context"
+	"slices"
 
 	sdk "github.com/pipe-cd/piped-plugin-sdk-go"
 
 	"github.com/pipe-cd/community-plugins/plugins/sqldef/config"
+)
+
+const (
+	// run sqldef with --dry-run
+	SqldefStagePlan  string = "SQLDEF_PLAN"
+	SqldefStageApply string = "SQLDEF_APPLY"
+	// by running with previous DB schema dump
+	SqldefStageRollback string = "SQLDEF_ROLLBACK"
 )
 
 // Plugin implements sdk.DeploymentPlugin for OpenTofu.
@@ -14,8 +23,11 @@ type Plugin struct{}
 var _ sdk.DeploymentPlugin[config.Config, config.DeployTargetConfig, config.ApplicationConfigSpec] = (*Plugin)(nil)
 
 func (*Plugin) FetchDefinedStages() []string {
-	// TODO: Implement FetchDefinedStages logic
-	return []string{}
+	return []string{
+		SqldefStagePlan,
+		SqldefStageApply,
+		SqldefStageRollback,
+	}
 }
 
 // BuildPipelineSyncStages builds the stages that will be executed by the plugin.
@@ -24,9 +36,33 @@ func (p *Plugin) BuildPipelineSyncStages(
 	cfg *config.Config,
 	input *sdk.BuildPipelineSyncStagesInput,
 ) (*sdk.BuildPipelineSyncStagesResponse, error) {
-	// TODO: Implement BuildPipelineSyncStages logic
+	reqStages := input.Request.Stages
+	out := make([]sdk.PipelineStage, 0, len(reqStages)+1)
+
+	for _, s := range reqStages {
+		out = append(out, sdk.PipelineStage{
+			Index:              s.Index,
+			Name:               s.Name,
+			Rollback:           false,
+			Metadata:           make(map[string]string),
+			AvailableOperation: sdk.ManualOperationNone,
+		})
+	}
+
+	if input.Request.Rollback {
+		// we set the index of the rollback stage to the minimum index of all stages.
+		minIndex := slices.MinFunc(reqStages, func(a, b sdk.StageConfig) int { return a.Index - b.Index }).Index
+		out = append(out, sdk.PipelineStage{
+			Name:               SqldefStageRollback,
+			Index:              minIndex,
+			Rollback:           true,
+			Metadata:           make(map[string]string),
+			AvailableOperation: sdk.ManualOperationNone,
+		})
+	}
+
 	return &sdk.BuildPipelineSyncStagesResponse{
-		Stages: []sdk.PipelineStage{},
+		Stages: out,
 	}, nil
 }
 
@@ -43,25 +79,25 @@ func (p *Plugin) ExecuteStage(
 	}, nil
 }
 
+// No need for the sqldef plugin
 // DetermineVersions determines the versions of artifacts for the deployment.
 func (p *Plugin) DetermineVersions(
 	ctx context.Context,
 	cfg *config.Config,
 	input *sdk.DetermineVersionsInput[config.ApplicationConfigSpec],
 ) (*sdk.DetermineVersionsResponse, error) {
-	// TODO: Implement DetermineVersions logic
 	return &sdk.DetermineVersionsResponse{
 		Versions: []sdk.ArtifactVersion{},
 	}, nil
 }
 
+// No need for the sqldef plugin
 // DetermineStrategy determines the strategy for the deployment.
 func (p *Plugin) DetermineStrategy(
 	ctx context.Context,
 	cfg *config.Config,
 	input *sdk.DetermineStrategyInput[config.ApplicationConfigSpec],
 ) (*sdk.DetermineStrategyResponse, error) {
-	// TODO: Implement DetermineStrategy logic
 	return nil, nil
 }
 
@@ -71,8 +107,26 @@ func (p *Plugin) BuildQuickSyncStages(
 	cfg *config.Config,
 	input *sdk.BuildQuickSyncStagesInput,
 ) (*sdk.BuildQuickSyncStagesResponse, error) {
-	// TODO: Implement BuildQuickSyncStages logic
+	stages := make([]sdk.QuickSyncStage, 0, 2)
+	stages = append(stages, sdk.QuickSyncStage{
+		Name:               SqldefStageApply,
+		Description:        "Apply all changes",
+		Rollback:           false,
+		Metadata:           map[string]string{},
+		AvailableOperation: sdk.ManualOperationNone,
+	})
+
+	if input.Request.Rollback {
+		stages = append(stages, sdk.QuickSyncStage{
+			Name:               SqldefStageRollback,
+			Description:        "Rollback to previous DB schema",
+			Rollback:           true,
+			Metadata:           map[string]string{},
+			AvailableOperation: sdk.ManualOperationNone,
+		})
+	}
+
 	return &sdk.BuildQuickSyncStagesResponse{
-		Stages: []sdk.QuickSyncStage{},
+		Stages: stages,
 	}, nil
 }
