bed3b3264bba8aad00da562dc220e680f05949b2
Chun Wei Lu <kadai0308@gmail.com>
2025-08-25 20:31:06 +1000

    [Sqldef Pluging] refactor(sqldef) refactor sqldef toolRegistry download binary (#36)
    * refactor(sqldef) refactor sqldef toolRegistry download binary
    
    Signed-off-by: kadai0308 <kadai0308@gmail.com>
    
    * refactor(sqldef) remove binary path cache from stage_plan
    
    Signed-off-by: kadai0308 <kadai0308@gmail.com>
    
    * refactor(sqldef) fix deployment/stage_plan_test.go
    
    Signed-off-by: kadai0308 <kadai0308@gmail.com>
    
    * refactor(sqldef) remove useless comment and add TODO to sqldef registry_test
    
    Signed-off-by: kadai0308 <kadai0308@gmail.com>
    
    ---------
    
    Signed-off-by: kadai0308 <kadai0308@gmail.com>

--- plugins/sqldef/deployment/stage_plan.go
+++ plugins/sqldef/deployment/stage_plan.go
@@ -2,7 +2,6 @@ package deployment
 
 import (
 	"context"
-	"fmt"
 	"github.com/pipe-cd/community-plugins/plugins/sqldef/config"
 	toolRegistryPkg "github.com/pipe-cd/community-plugins/plugins/sqldef/toolregistry"
 
@@ -16,9 +15,6 @@ func (p *Plugin) executePlanStage(ctx context.Context, dts []*sdk.DeployTarget[c
 	// Currently, we create them every time the stage is executed beucause we can't pass input.Client.toolRegistry to the plugin when starting the plugin.
 	toolRegistry := toolRegistryPkg.NewRegistry(input.Client.ToolRegistry())
 
-	// map for prevent repeatedly download sqldef
-	downloadedSqldefPaths := make(map[config.DBType]string)
-
 	for _, dt := range dts {
 		lp.Infof("Deploy Target [%s]: host=%s, port=%s, db=%s, schemaFile=%s",
 			dt.Name,
@@ -27,26 +23,10 @@ func (p *Plugin) executePlanStage(ctx context.Context, dts []*sdk.DeployTarget[c
 			dt.Config.DBName,
 		)
 
-		// check db_type from dt.config to choose which sqldef binary to download
-		// Now we only support mysql temporarily
-		var sqlDefPath string
-		var existed bool
-		if sqlDefPath, existed = downloadedSqldefPaths[config.DBTypeMySQL]; !existed {
-			var binPath string
-			var err error
-			switch dt.Config.DbType {
-			case config.DBTypeMySQL:
-				binPath, err = toolRegistry.Mysqldef(ctx, "")
-			default:
-				lp.Errorf(fmt.Sprintf("Unsupported database type: %s, currently only support: mysql", dt.Name))
-				return sdk.StageStatusFailure
-			}
-			if err != nil {
-				lp.Errorf("Failed while getting Sqldef tool (%v)", err)
-				return sdk.StageStatusFailure
-			}
-			downloadedSqldefPaths[dt.Config.DbType] = binPath
-			lp.Info(fmt.Sprintf("Sqldef binary downloadeded: %s", sqlDefPath))
+		sqlDefPath, err := toolRegistry.DownloadBinary(ctx, dt.Config.DbType, "")
+		if err != nil {
+			lp.Errorf("Failed while getting Sqldef tool (%v)", err)
+			return sdk.StageStatusFailure
 		}
 
 		appDir := input.Request.RunningDeploymentSource.ApplicationDirectory
